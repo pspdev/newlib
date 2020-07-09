a7274a3c3b4120449da7decc78e99a54d3377c31
Carsten Teibes <dev@f4ke.de>
2020-07-09 03:13:18 +0200

    Do not use unavailable builtins

--- newlib/libc/sys/psp/netinet/in.h
+++ newlib/libc/sys/psp/netinet/in.h
@@ -259,10 +259,19 @@ struct ip_mreq {
 #define	IP_PORTRANGE_HIGH	1	/* same as DEFAULT (FreeBSD compat) */
 #define	IP_PORTRANGE_LOW	2	/* use privileged range */
 
+
+#if 0
+// FIXME: These are optimized (one instruction), but currently not part of psp-gcc
 #define ntohs(x) __builtin_allegrex_wsbh(x)
 #define ntohl(x) __builtin_allegrex_wsbw(x)
 #define htons(x) __builtin_allegrex_wsbh(x)
 #define htonl(x) __builtin_allegrex_wsbw(x)
+#else
+#define ntohs(x) __builtin_bswap16(x)
+#define ntohl(x) __builtin_bswap32(x)
+#define htons(x) __builtin_bswap16(x)
+#define htonl(x) __builtin_bswap32(x)
+#endif
 
 #ifdef __cplusplus
 }
