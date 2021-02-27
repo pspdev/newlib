91aa9adc67e6d4d9c67b3b6906a6bc21c5f3fdee
Francisco Javier Trujillo Mata <fran.t.es@sync.money>
2021-02-27 12:31:03 +0100

    Making a clean PSP port
    - Add missing fields to dirent
    - Add _POSIX_MONOTONIC_CLOCK to features
    - Add statvfs file

--- newlib/libc/include/sys/features.h
+++ newlib/libc/include/sys/features.h
@@ -545,6 +545,11 @@ extern "C" {
 
 #endif /* __CYGWIN__ */
 
+#ifdef _MIPS_ARCH_ALLEGREX
+# define _POSIX_TIMERS				1
+# define _POSIX_MONOTONIC_CLOCK			200112L
+#endif
+
 #ifdef __cplusplus
 }
 #endif
