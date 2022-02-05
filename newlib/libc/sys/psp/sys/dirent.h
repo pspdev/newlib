b9c77d5302e4bb56196127fed1ca06fb6ee75a01
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2022-02-05 20:51:47 +0100

    Add missing fields to dirent

--- newlib/libc/sys/psp/sys/dirent.h
+++ newlib/libc/sys/psp/sys/dirent.h
@@ -20,9 +20,22 @@ typedef struct __dirdesc {
 #define d_ino	d_fileno	/* compatibility */
 
 struct dirent {
-	ino_t      d_fileno;		/* file number of entry */
-	unsigned short int d_reclen;
+	ino_t      d_fileno;		    /* file number of entry */
+    off_t          d_off;           /* Not an offset; see below */
+	unsigned short int d_reclen;    /* Length of this record */
+    unsigned char  d_type;          /* Type of file; not supported 
+                                       by all filesystem types */
 	char d_name[MAXNAMLEN + 1];
 };
+
+#define	DT_UNKNOWN	 0
+#define	DT_FIFO		 1
+#define	DT_CHR		 2
+#define	DT_DIR		 4
+#define	DT_BLK		 6
+#define	DT_REG		 8
+#define	DT_LNK		10
+#define	DT_SOCK		12
+#define	DT_WHT		14
 
 #endif
